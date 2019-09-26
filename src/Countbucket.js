de4d0b1565bb8f0c5b48f9311b7662183ea94cc7
mnmr1996 <mnmr1996@gmail.com>
2019-09-26 15:43:30 -0400

    formatted the code with prettier

--- src/Countbucket.js
+++ src/Countbucket.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import Review from './Review'
+import React from "react";
+import Review from "./Review";
 
-
-const Countbucket = props => {
-    return(
-        <div>
-            <h2>{props.Month} {props.Year}</h2>
-            {props.array.length > 0 && 
-      props.array.map(userReviews => (
-        <div>
-          <Review author={userReviews.author} review= {userReviews.review} stars={userReviews.stars} id={userReviews.id} date={userReviews.date} title={userReviews.title}/>
-        </div>
-        ))}
-        </div>
-    )
-}
+const Countbucket = (props) => {
+	return (
+		<div>
+			<h2>
+				{props.Month} {props.Year}
+			</h2>
+			{props.array.length > 0 &&
+				props.array.map((userReviews) => (
+					<div>
+						<Review
+							author={userReviews.author}
+							review={userReviews.review}
+							stars={userReviews.stars}
+							id={userReviews.id}
+							date={userReviews.date}
+							title={userReviews.title}
+						/>
+					</div>
+				))}
+		</div>
+	);
+};
 
 export default Countbucket;
